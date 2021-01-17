4b8e3084e0871ab2b0677c8ca10bb27ec0c66ed7
Andreas Heimann <privat@andreas-heimann.com>
2021-01-17 18:12:35 +0100

    bubbles for way

--- src/components/Screens/ScreenCommentators2.vue
+++ src/components/Screens/ScreenCommentators2.vue
@@ -19,6 +19,24 @@
             </div>
         </div>
 
+        <div class="blobs">
+            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                <path d="M48.5,-16.2C55.2,4.7,47.5,29.8,30.1,42.7C12.7,55.6,-14.4,56.4,-29.9,44.4C-45.5,32.3,-49.4,7.5,-42.5,-13.7C-35.6,-35,-17.8,-52.6,1.6,-53.1C20.9,-53.6,41.9,-37,48.5,-16.2Z" transform="translate(100 100)" />
+            </svg>
+            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                <path d="M72,-22.7C81.3,5.3,68.7,41.2,43.9,58.9C19.1,76.5,-17.9,76,-33.9,60.9C-50,45.9,-45.1,16.4,-36,-11.4C-26.9,-39.2,-13.4,-65.3,8.9,-68.2C31.3,-71.1,62.7,-50.8,72,-22.7Z" transform="translate(100 100)" />
+            </svg>
+            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                <path d="M52.8,-18.5C60,5.2,51.8,32.6,34.6,44.3C17.4,56,-8.8,52,-24.2,39.4C-39.7,26.7,-44.5,5.3,-38.7,-16.4C-32.8,-38.1,-16.4,-60.2,3.2,-61.3C22.8,-62.3,45.5,-42.2,52.8,-18.5Z" transform="translate(100 100)" />
+            </svg>
+            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                <path d="M35.4,-7.2C42.9,11.7,44,36.9,29.7,49.3C15.4,61.7,-14.5,61.3,-30.1,48.3C-45.7,35.3,-47.2,9.8,-39.8,-8.9C-32.4,-27.5,-16.2,-39.1,-1.1,-38.8C13.9,-38.4,27.9,-26,35.4,-7.2Z" transform="translate(100 100)" />
+            </svg>
+            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                <path d="M62.3,-23.6C69.7,2.5,57,31.8,37.2,44.7C17.4,57.6,-9.7,54,-28.9,40C-48.1,26,-59.4,1.5,-53.3,-22.8C-47.2,-47.1,-23.6,-71.3,1.9,-71.9C27.4,-72.5,54.9,-49.6,62.3,-23.6Z" transform="translate(100 100)" />
+            </svg>
+        </div>
+
         <div class="music">
             <span class="mdi mdi-music"></span>
             <span><strong>{{ snipAuthor }}</strong>{{ snipTitle }}</span>
@@ -85,6 +103,43 @@
         bottom: 0px;
         z-index: 10;
         display: grid;
+        
+        & .blobs {
+            position: absolute;
+            left: 0px;
+            right: 0px;
+            top: 0px;
+            bottom: 0px;
+
+            & svg {
+                position: absolute;
+                width: 2vw;
+                height: 2vw;
+                animation: blobAnimSmall 10s infinite alternate linear;
+                transition: 2s ease all;
+
+                &:nth-child(2n) {
+                    width: 4vw;
+                    height: 4vw;
+                    animation: blobAnimMedium 15s infinite alternate linear;
+                }
+                &:nth-child(3n) {
+                    width: 6vw;
+                    height: 6vw;
+                    animation: blobAnimBig 20s infinite alternate linear;
+                }
+
+                // Top Left
+                &:nth-child(1) { top: 2vh; left: 5vw; animation-delay: -1s; }
+
+                // Bottom left
+                &:nth-child(2) { top: 70vh; left: 2vw; animation-delay: -2s; }
+                &:nth-child(3) { top: 70vh; left: 80vw; animation-delay: -3s; }
+                // Bottom Left
+                &:nth-child(4) { top: 75vh; left: 10vw; animation-delay: -4s; }
+                &:nth-child(5) { top: 88vh; left: 4vw; animation-delay: -5s; }
+            }
+        }
 
         & .chat-box {
             background: #000;
