1600a4f6a5b429e82b54667cc5cd7f2b7b944cea
rjstech <49792255+rjstech@users.noreply.github.com>
2022-01-08 15:13:37 +0000

    Need le GQL :(

--- src/views/Controls/ControlBrackets.vue
+++ src/views/Controls/ControlBrackets.vue
@@ -22,15 +22,15 @@
     </InputGroup>
     
     <InputGroup title="Phase Group (Qualifiers Only)">
-      <select id="selectedQualiGroup" @change="selectCategory($groupSel)">
-        <option value="1581602">(7) swagdude, Daquat, Abysmal Cosmos, AngryScootsman</option>
-        <option value="1581599">(4) Mapy, tetrachris, oproerling, CaliCalicko</option>
-        <option value="1581592">(1) TreXDer, Kwazi, haomakk, Avri</option>
-        <option value="1581601">(6) PnO_Mader, metalman20, Halflite, CarbonCarbon12</option>
-        <option value="1581597">(2) Ricki, GaviGuy, Franco, Drogin_dunlane</option>
-        <option value="1581603">(8) Konomi, Ballinbino, Cahobo, Nemo</option>
-        <option value="1581600">(5) Pick, Krauvando, Gamer97, FlyinPoulpus</option>
-        <option value="1581598">(3) Programmatic, Edge, Loosiano, Aexus</option>
+      <select id="selectedQualiGroup" v-model="QualiSel">
+        <option value="1581602" v-on:click="updateData(QualiSel=1581602)">(7) swagdude, Daquat, Abysmal Cosmos, AngryScootsman</option>
+        <option value="1581599" v-on:click="updateData(QualiSel=1581599)">(4) Mapy, tetrachris, oproerling, CaliCalicko</option>
+        <option value="1581592" v-on:click="updateData(QualiSel=1581592)">(1) TreXDer, Kwazi, haomakk, Avri</option>
+        <option value="1581601" v-on:click="updateData(QualiSel=1581601)">(6) PnO_Mader, metalman20, Halflite, CarbonCarbon12</option>
+        <option value="1581597" v-on:click="updateData(QualiSel=1581597)">(2) Ricki, GaviGuy, Franco, Drogin_dunlane</option>
+        <option value="1581603" v-on:click="updateData(QualiSel=1581603)">(8) Konomi, Ballinbino, Cahobo, Nemo</option>
+        <option value="1581600" v-on:click="updateData(QualiSel=1581600)">(5) Pick, Krauvando, Gamer97, FlyinPoulpus</option>
+        <option value="1581598" v-on:click="updateData(QualiSel=1581598)">(3) Programmatic, Edge, Loosiano, Aexus</option>
       </select>
     </InputGroup>
 
@@ -44,9 +44,15 @@ import { remote, ipcRenderer } from "electron";
 import Fab from "@/components/Controls/Fab.vue";
 import InputGroup from "@/components/Controls/InputGroup.vue";
 
-const smashgg = require('smashgg.js');
-const PhaseGroup = smashgg.PhaseGroup;
-smashgg.initialize('d71313e118d959f25ffe64818ffac164');
+const yourServerUrl  = 'https://api.smash.gg/gql/alpha/';
+const yourQuery = {
+  query: `{
+          id: 1581602,
+          page: 1,
+          perPage: 3
+      }`
+  };
+
 
 export default {
   name: "ControlBrackets",
@@ -56,9 +62,11 @@ export default {
   },
   data: function () {
     return {
+      QualiSel: 0,
+      
       topLeftText: "BRACKETS",
-
-      r1p1name: selectedPhase,
+      
+      r1p1name: "Round 1 Player 1",
       r1p1score: 0,
       r1p2name: "Round 1 Player 2",
       r1p2score: 0,
@@ -86,16 +94,26 @@ export default {
   },
   mounted: function () {},
   methods: {
-      selectCategory(groupSel) {
-      let selectedPhase = groupSel.target.value;
-    },
+ 
+    getSmashGroupData: function(){
+      const xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      xhr.open('GET', yourServerUrl);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader("Authorization", "Bearer f698e3aa0160363d4e4e880ac98fa3a1");
+      xhr.onload = function () {
+          console.log('data returned:', xhr.response);
+      };
+      xhr.send(JSON.stringify(yourQuery));
+     
+     
+     /*let phaseGroup1 = await PhaseGroup.get(this.$data.QualiSel);
+      let phaseSets = phaseGroup1.getSets();
+      console.log(phaseGroup1);
+      for(var i in phaseSets){
+          console.log(`${String(sets[i].getFullRoundText()).magenta}: ${String(sets[i].getDisplayScore()).green}`);
+      }*/
 
-    getSmashGroupData: async function(){
-      let phaseGroup1 = await PhaseGroup.get(1581602);
-      
-
-
-      return true;
     },
 
     transition: function () {
@@ -107,7 +125,7 @@ export default {
       console.log("[Controls] Update BracketsData");
       ipcRenderer.send("update-bracketsData", {
         topLeftText: this.$data.topLeftText,
-        r1p1name: this.$data.r1p1name,
+        r1p1name: this.$data.QualiSel,
         r1p1score: this.$data.r1p1score,
         r1p2name: this.$data.r1p2name,
         r1p2score: this.$data.r1p2score,
