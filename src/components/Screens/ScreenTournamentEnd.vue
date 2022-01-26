f6c8a6aa3cd1e984c7eae0af3113bfe852d32bac
waywtc123 <waywtc123@gmail.com>
2022-01-26 02:23:46 -0400

    Update ScreenTournamentEnd.vue

--- src/components/Screens/ScreenTournamentEnd.vue
+++ src/components/Screens/ScreenTournamentEnd.vue
@@ -43,7 +43,7 @@
                 <div class="username">{{ winnerData.username }}</div>
                 <div class="title">
                     SpinShare SpeenOpen<br />
-                    Winter 2022 Challenger Winner
+                    Winter 2022 Elite Winner
                 </div>
             </div>
         </div>
