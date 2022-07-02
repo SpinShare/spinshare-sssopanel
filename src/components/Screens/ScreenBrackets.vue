4f469176ba0482ad5a11090b90aac28a1e350e45
rjstech <49792255+rjstech@users.noreply.github.com>
2022-07-02 02:59:37 +0100

    Re-establish maybe

--- src/components/Screens/ScreenBrackets.vue
+++ src/components/Screens/ScreenBrackets.vue
@@ -2,34 +2,34 @@
     <div class="screenBrackets">
         <div class="blobs">
             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#23B0E2" d="M48.5,-16.2C55.2,4.7,47.5,29.8,30.1,42.7C12.7,55.6,-14.4,56.4,-29.9,44.4C-45.5,32.3,-49.4,7.5,-42.5,-13.7C-35.6,-35,-17.8,-52.6,1.6,-53.1C20.9,-53.6,41.9,-37,48.5,-16.2Z" transform="translate(100 100)" />
+                <path fill="#27E277" d="M48.5,-16.2C55.2,4.7,47.5,29.8,30.1,42.7C12.7,55.6,-14.4,56.4,-29.9,44.4C-45.5,32.3,-49.4,7.5,-42.5,-13.7C-35.6,-35,-17.8,-52.6,1.6,-53.1C20.9,-53.6,41.9,-37,48.5,-16.2Z" transform="translate(100 100)" />
             </svg>
             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#23B0E2" d="M72,-22.7C81.3,5.3,68.7,41.2,43.9,58.9C19.1,76.5,-17.9,76,-33.9,60.9C-50,45.9,-45.1,16.4,-36,-11.4C-26.9,-39.2,-13.4,-65.3,8.9,-68.2C31.3,-71.1,62.7,-50.8,72,-22.7Z" transform="translate(100 100)" />
+                <path fill="#27E277" d="M72,-22.7C81.3,5.3,68.7,41.2,43.9,58.9C19.1,76.5,-17.9,76,-33.9,60.9C-50,45.9,-45.1,16.4,-36,-11.4C-26.9,-39.2,-13.4,-65.3,8.9,-68.2C31.3,-71.1,62.7,-50.8,72,-22.7Z" transform="translate(100 100)" />
             </svg>
             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#23B0E2" d="M52.8,-18.5C60,5.2,51.8,32.6,34.6,44.3C17.4,56,-8.8,52,-24.2,39.4C-39.7,26.7,-44.5,5.3,-38.7,-16.4C-32.8,-38.1,-16.4,-60.2,3.2,-61.3C22.8,-62.3,45.5,-42.2,52.8,-18.5Z" transform="translate(100 100)" />
+                <path fill="#27E277" d="M52.8,-18.5C60,5.2,51.8,32.6,34.6,44.3C17.4,56,-8.8,52,-24.2,39.4C-39.7,26.7,-44.5,5.3,-38.7,-16.4C-32.8,-38.1,-16.4,-60.2,3.2,-61.3C22.8,-62.3,45.5,-42.2,52.8,-18.5Z" transform="translate(100 100)" />
             </svg>
             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#23B0E2" d="M35.4,-7.2C42.9,11.7,44,36.9,29.7,49.3C15.4,61.7,-14.5,61.3,-30.1,48.3C-45.7,35.3,-47.2,9.8,-39.8,-8.9C-32.4,-27.5,-16.2,-39.1,-1.1,-38.8C13.9,-38.4,27.9,-26,35.4,-7.2Z" transform="translate(100 100)" />
+                <path fill="#27E277" d="M35.4,-7.2C42.9,11.7,44,36.9,29.7,49.3C15.4,61.7,-14.5,61.3,-30.1,48.3C-45.7,35.3,-47.2,9.8,-39.8,-8.9C-32.4,-27.5,-16.2,-39.1,-1.1,-38.8C13.9,-38.4,27.9,-26,35.4,-7.2Z" transform="translate(100 100)" />
             </svg>
             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#23B0E2" d="M62.3,-23.6C69.7,2.5,57,31.8,37.2,44.7C17.4,57.6,-9.7,54,-28.9,40C-48.1,26,-59.4,1.5,-53.3,-22.8C-47.2,-47.1,-23.6,-71.3,1.9,-71.9C27.4,-72.5,54.9,-49.6,62.3,-23.6Z" transform="translate(100 100)" />
+                <path fill="#27E277" d="M62.3,-23.6C69.7,2.5,57,31.8,37.2,44.7C17.4,57.6,-9.7,54,-28.9,40C-48.1,26,-59.4,1.5,-53.3,-22.8C-47.2,-47.1,-23.6,-71.3,1.9,-71.9C27.4,-72.5,54.9,-49.6,62.3,-23.6Z" transform="translate(100 100)" />
             </svg>
             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#23B0E2" d="M54.5,-8.9C62.7,7.5,55.9,37.6,37.1,51.4C18.2,65.2,-12.8,62.7,-31.6,48C-50.3,33.3,-56.9,6.3,-49.6,-8.9C-42.3,-24.1,-21.2,-27.6,1,-27.9C23.1,-28.2,46.3,-25.4,54.5,-8.9Z" transform="translate(100 100)" />
+                <path fill="#27E277" d="M54.5,-8.9C62.7,7.5,55.9,37.6,37.1,51.4C18.2,65.2,-12.8,62.7,-31.6,48C-50.3,33.3,-56.9,6.3,-49.6,-8.9C-42.3,-24.1,-21.2,-27.6,1,-27.9C23.1,-28.2,46.3,-25.4,54.5,-8.9Z" transform="translate(100 100)" />
             </svg>
             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#23B0E2" d="M36,-3.7C44.9,15.8,49.3,44.8,35.4,56.7C21.6,68.6,-10.4,63.5,-29.1,48C-47.8,32.6,-53.3,6.7,-46.2,-10.2C-39.1,-27.1,-19.6,-35.1,-3,-34.1C13.5,-33.2,27.1,-23.2,36,-3.7Z" transform="translate(100 100)" />
+                <path fill="#27E277" d="M36,-3.7C44.9,15.8,49.3,44.8,35.4,56.7C21.6,68.6,-10.4,63.5,-29.1,48C-47.8,32.6,-53.3,6.7,-46.2,-10.2C-39.1,-27.1,-19.6,-35.1,-3,-34.1C13.5,-33.2,27.1,-23.2,36,-3.7Z" transform="translate(100 100)" />
             </svg>
             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#23B0E2" d="M25.7,-4.8C33.8,16.6,41.2,41.7,28.2,54.9C15.3,68.2,-18.1,69.4,-36.6,54.9C-55.1,40.4,-58.9,10,-50.3,-12C-41.8,-34,-20.9,-47.5,-6,-45.6C8.8,-43.6,17.6,-26.1,25.7,-4.8Z" transform="translate(100 100)" />
+                <path fill="#27E277" d="M25.7,-4.8C33.8,16.6,41.2,41.7,28.2,54.9C15.3,68.2,-18.1,69.4,-36.6,54.9C-55.1,40.4,-58.9,10,-50.3,-12C-41.8,-34,-20.9,-47.5,-6,-45.6C8.8,-43.6,17.6,-26.1,25.7,-4.8Z" transform="translate(100 100)" />
             </svg>
             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#23B0E2" d="M33.2,-15.6C36.9,0.7,29.6,15.7,14.8,28.4C-0.1,41.1,-22.5,51.5,-34.9,43.6C-47.2,35.6,-49.5,9.4,-42,-12C-34.5,-33.4,-17.3,-50,-1.3,-49.6C14.7,-49.2,29.5,-31.8,33.2,-15.6Z" transform="translate(100 100)" />
+                <path fill="#27E277" d="M33.2,-15.6C36.9,0.7,29.6,15.7,14.8,28.4C-0.1,41.1,-22.5,51.5,-34.9,43.6C-47.2,35.6,-49.5,9.4,-42,-12C-34.5,-33.4,-17.3,-50,-1.3,-49.6C14.7,-49.2,29.5,-31.8,33.2,-15.6Z" transform="translate(100 100)" />
             </svg>
             <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#23B0E2" d="M69.9,-20.4C78.8,4.7,66.1,39.1,44.9,52.2C23.8,65.3,-5.8,57,-23.6,41.9C-41.3,26.8,-47.1,4.8,-41.2,-16.2C-35.3,-37.1,-17.6,-57,6.4,-59.1C30.5,-61.2,61,-45.5,69.9,-20.4Z" transform="translate(100 100)" />
+                <path fill="#27E277" d="M69.9,-20.4C78.8,4.7,66.1,39.1,44.9,52.2C23.8,65.3,-5.8,57,-23.6,41.9C-41.3,26.8,-47.1,4.8,-41.2,-16.2C-35.3,-37.1,-17.6,-57,6.4,-59.1C30.5,-61.2,61,-45.5,69.9,-20.4Z" transform="translate(100 100)" />
             </svg>
         </div>
 
@@ -179,74 +179,74 @@
                 snipAuthor: "",
 				topLeftText: "BRACKETS",
 
-                r1p1name: "Kwazi",
-                r1p1score: 0,
-                r1p2name: "ballinbino",
+                r1p1name: "TreXDer",
+                r1p1score: 3,
+                r1p2name: "Konomi",
                 r1p2score: 0,
 
-                r2p1name: "oproeling",
-                r2p1score: 0,
-                r2p2name: "Krauvando",
-                r2p2score: 0,
+                r2p1name: "Mapy",
+                r2p1score: 3,
+                r2p2name: "Pick",
+                r2p2score: 2,
 
-                r3p1name: "Ricki",
-                r3p1score: 0,
-                r3p2name: "AbysmalCosmos",
-                r3p2score: 0,
+                r3p1name: "GaviGuy",
+                r3p1score: 2,
+                r3p2name: "Daquat",
+                r3p2score: 3,
 
-                r4p1name: "Loosiano",
+                r4p1name: "Programmatic",
                 r4p1score: 0,
-                r4p2name: "metalman20",
-                r4p2score: 0,
+                r4p2name: "PnO_Mader",
+                r4p2score: 3,
 
-                wsf1p1name: "Winner of QF 1",
-                wsf1p1score: 0,
-                wsf1p2name: "Winner of QF 2",
-                wsf1p2score: 0,
+                wsf1p1name: "TreXDer",
+                wsf1p1score: 4,
+                wsf1p2name: "Mapy",
+                wsf1p2score: 1,
 
-                wsf2p1name: "Winner of QF 3",
-                wsf2p1score: 0,
-                wsf2p2name: "Winner of QF 4",
-                wsf2p2score: 0,
+                wsf2p1name: "Daquat",
+                wsf2p1score: 2,
+                wsf2p2name: "PnO_Mader",
+                wsf2p2score: 4,
 
-                wfp1name: "Winner of SF 1",
-                wfp1score: 0,
-                wfp2name: "Winner of SF 2",
-                wfp2score: 0,
+                wfp1name: "TreXDer",
+                wfp1score: 5,
+                wfp2name: "PnO_Mader",
+                wfp2score: 2,
 
-                gfp1name: "Winner of WF",
+                gfp1name: "TreXDer",
                 gfp1score: 0,
                 gfp2name: "Winner of LF",
                 gfp2score: 0,
 
-                lr2p1name: "Loser of QF 1",
+                lr2p1name: "Konomi",
                 lr2p1score: 0,
-                lr2p2name: "Loser of QF 2",
-                lr2p2score: 0,
+                lr2p2name: "Pick",
+                lr2p2score: 3,
                 
-                lr1p1name: "Loser of QF 3",
+                lr1p1name: "GaviGuy",
                 lr1p1score: 0,
-                lr1p2name: "Loser of QF 4",
-                lr1p2score: 0,
+                lr1p2name: "Programmatic",
+                lr1p2score: 3,
                 
-                lqf2p1name: "Loser of SF 2",
-                lqf2p1score: 0,
-                lqf2p2name: "Winner of LR 1",
-                lqf2p2score: 0,
+                lqf2p1name: "Daquat",
+                lqf2p1score: 2,
+                lqf2p2name: "Pick",
+                lqf2p2score: 3,
 
-                lqf1p1name: "Loser of SF 1",
-                lqf1p1score: 0,
-                lqf1p2name: "Winner of LR 2",
-                lqf1p2score: 0,
+                lqf1p1name: "Mapy",
+                lqf1p1score: 3,
+                lqf1p2name: "Programmatic",
+                lqf1p2score: 2,
 
-                lsfp1name: "Winner of LQF 1",
-                lsfp1score: 0,
-                lsfp2name: "Winner of LQF 2",
+                lsfp1name: "Pick",
+                lsfp1score: 4,
+                lsfp2name: "Mapy",
                 lsfp2score: 0,
                 
-                lfp1name: "Loser of WF",
+                lfp1name: "PnO_Mader",
                 lfp1score: 0,
-                lfp2name: "Winner of LSF",
+                lfp2name: "Pick",
                 lfp2score: 0,                
             }
         },
@@ -678,7 +678,7 @@ setText("lose-r2-p2-score","1");*/
                     left:77.5%;
                     top:24%;
                     width:22.5%;
-                    background: #99ceff;
+                    background: #9bff99;
                     border-radius: 0.75vh;
                     color: #000;
                     padding: 1vh 1vw;
--- src/views/Controls/ControlBrackets.vue
+++ src/views/Controls/ControlBrackets.vue
@@ -2,36 +2,15 @@
   <div class="controlBrackets">
     <Fab v-on:click.native="transition()" title="Transition" />
 
-    <!-- <InputGroup title="Toornament Event ID">
+    <InputGroup title="Toornament Event ID">
       <input type="number" v-model="toornamentEventId" />
     </InputGroup>
 
     <InputGroup title="Toornament Stage ID">
       <input type="number" v-model="toornamentStageId" />
     </InputGroup>
-    <InputGroup title="Top Left Text">
-      <input type="string" v-model="topLeftText" placeholder="GROUP STAGE" />
-    </InputGroup>
--->
-    <InputGroup title="Phase">
-      <select id="selectedPhase">
-        <option value="1002204">Qualifiers</option>
-        <option value="1002220">Challenger Playoffs</option>
-        <option value="1002212">Elite Playoffs</option>
-      </select>
-    </InputGroup>
-    
-    <InputGroup title="Phase Group (Qualifiers Only)">
-      <select id="selectedQualiGroup" v-model="QualiSel">
-        <option value="1581602" v-on:click="updateData(QualiSel=1581602)">(7) swagdude, Daquat, Abysmal Cosmos, AngryScootsman</option>
-        <option value="1581599" v-on:click="updateData(QualiSel=1581599)">(4) Mapy, tetrachris, oproerling, CaliCalicko</option>
-        <option value="1581592" v-on:click="updateData(QualiSel=1581592)">(1) TreXDer, Kwazi, haomakk, Avri</option>
-        <option value="1581601" v-on:click="updateData(QualiSel=1581601)">(6) PnO_Mader, metalman20, Halflite, CarbonCarbon12</option>
-        <option value="1581597" v-on:click="updateData(QualiSel=1581597)">(2) Ricki, GaviGuy, Franco, Drogin_dunlane</option>
-        <option value="1581603" v-on:click="updateData(QualiSel=1581603)">(8) Konomi, Ballinbino, Cahobo, Nemo</option>
-        <option value="1581600" v-on:click="updateData(QualiSel=1581600)">(5) Pick, Krauvando, Gamer97, FlyinPoulpus</option>
-        <option value="1581598" v-on:click="updateData(QualiSel=1581598)">(3) Programmatic, Edge, Loosiano, Aexus</option>
-      </select>
+    <InputGroup title="Phase Group ID">
+      <input type="number" v-model="phaseID" placeholder="PhaseGroupID" />
     </InputGroup>
 
     <button v-on:click="updateData()">Update</button>
@@ -39,41 +18,10 @@
 </template>
 
 <script>
-const axios = require('axios').default;
-let smashData = null;
 import { remote, ipcRenderer } from "electron";
 
 import Fab from "@/components/Controls/Fab.vue";
 import InputGroup from "@/components/Controls/InputGroup.vue";
-
-const yourServerUrl  = 'https://api.smash.gg/gql/alpha/';
-const yourQuery = {
-  query: `{phaseGroup(id:1581602){
-              id
-              displayIdentifier
-              sets(
-                page: 1
-                perPage: 20
-                sortType: STANDARD
-              ){
-                pageInfo{
-                  total
-                }
-                nodes{
-                  id
-                  slots{
-                    id
-                    entrant{
-                      id
-                      name
-                    }
-                  }
-                }
-              }
-            }
-          }`
-  };
-
 
 export default {
   name: "ControlBrackets",
@@ -83,101 +31,92 @@ export default {
   },
   data: function () {
     return {
-      QualiSel: 0,
-      
-      topLeftText: "BRACKETS",
-      
-      r1p1name: "Round 1 Player 1",
-      r1p1score: 0,
-      r1p2name: "Round 1 Player 2",
-      r1p2score: 0,
-
-      r2p1name: "YES Round 2 Player 1",
-      r2p1score: 0,
-      r2p2name: "NO Round 2 Player 2",
-      r2p2score: 0,
-
-      winr2p1name: "Winner of Round 1",
-      winr2p1score: 0,
-      winr2p2name: "Winner of Round 2",
-      winr2p2score: 0,
-
-      loser1p1name: "Loser of Round 1",
-      loser1p1score: 0,
-      loser1p2name: "Loser of Round 2",
-      loser1p2score: 0,
-
-      loser2p1name: "Loser of Winners Round",
-      loser2p1score: 0,
-      loser2p2name: "Winner of Losers Round",
-      loser2p2score: 0,
+      toornamentEventId: 0,
+      toornamentStageId: 0,
+      phaseID: 0,
     };
   },
   mounted: function () {},
   methods: {
- 
-    getSmashGroupData: function(){
-      const query = `query PhaseGroupSets{
-        phaseGroup(id:1581602){
-          id
-          displayIdentifier
-          sets(
-            page: 1
-            perPage: 20
-            sortType: STANDARD
-          ){
-            pageInfo{
-              total
-            }
-            nodes{
-              id
-              slots{
-                id
-                entrant{
-                  id
-                  name
-                }
-              }
-            }
+    callStartggAPI: function (pg){
+      'use strict';
+      var request = require('request');
+      var url = 'https://api.start.gg/phase_group/1581602?expand[]=sets&expand[]=seeds';
+
+      request.get({
+          url: url,
+          json: true,
+          headers: {'User-Agent': 'request'}
+        }, (err, res, data) => {
+          if (err) {
+            console.log('Error:', err);
+          } else if (res.statusCode !== 200) {
+            console.log('Status:', res.statusCode);
+          } else {
+            // data is already parsed as JSON:
+            console.log(data.html_url);
           }
-        }
-      }`
-      const jsonQuery = JSON.stringify({ query });
-      const res = axios.post('https://api.smash.gg/gql/alpha', {
-        headers: {
-          'Authorization': `Bearer d71313e118d959f25ffe64818ffac164`,
-          'Content-Type': 'application/json',
-          'Access-Control-Request-Method': 'POST',
-          'Access-Control-Allow-Origin': '*',
-          'Accept': 'application/json',
-          
-        },
-        body: jsonQuery
       });
-      smashData = res;
-
-      //res.data.data; // '{"answer":42}'
-      //res.data.headers['Content-Type', 'Authorization']; // 'application/json',
       
-      /*const xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      xhr.open('POST', yourServerUrl);
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.setRequestHeader(Access-Control-Allow-Origin, '*');
-      xhr.setRequestHeader("Authorization", "Bearer f698e3aa0160363d4e4e880ac98fa3a1");
-      xhr.onload = function () {
-          console.log('data returned:', xhr.response);
-      };
-      xhr.send(JSON.stringify(yourQuery));*/
-     
-     
-     /*let phaseGroup1 = await PhaseGroup.get(this.$data.QualiSel);
-      let phaseSets = phaseGroup1.getSets();
-      console.log(phaseGroup1);
-      for(var i in phaseSets){
-          console.log(`${String(sets[i].getFullRoundText()).magenta}: ${String(sets[i].getDisplayScore()).green}`);
-      }*/
+      /*const https = require('https');
 
+      const options = {
+        hostname: 'api.start.gg',
+        port: 443,
+        path: '/phase_group/'& pg &'?expand[]=sets&expand[]=seeds',
+        method: 'GET',
+      };
+      
+      const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`);
+        let body = '';
+
+        res.on('data', function(chunk){
+          body += chunk;
+        });
+
+        res.on('end', function(){
+          let jsonResponse = JSON.parse(body);
+          console.log("Response:", jsonResponse);
+        });
+      });
+
+      req.on('error', error => {
+        console.error(error);
+      });
+
+      req.end();    */  
+
+      /*fetch("https://api.start.gg",{
+        "options": {
+          "Access-Control-Allow-Origin": "*",
+        },
+        "headers": {
+          "authority":"api.start.gg",
+          "method":"GET",
+          "path":"/phase_group/"& pg &"?expand[]=sets&expand[]=seeds",
+          "scheme":"https",
+          "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*//*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "accept-encoding":"gzip, deflate, br",
+          "accept-language":"en-GB,en-US;q=0.9,en;q=0.8",
+          "cache-control":"no-cache",
+          }
+      })
+      .then(response => { 
+          if(response.ok){
+            console.log(response);
+              //return response
+              return response.json()    
+          } else{
+              alert("Server returned " + response.status + " : " + response.statusText);
+          }                
+      })
+      .then(response => {
+          this.result = response.body; 
+      })
+      .catch(err => {
+          console.log(err);
+      });*/
     },
 
     transition: function () {
@@ -185,34 +124,15 @@ export default {
     },
 
     updateData: function () {
-      this.getSmashGroupData();
       console.log("[Controls] Update BracketsData");
+
+      //phaseID = this.$data.phaseID;
+      this.callStartggAPI(this.$data.phaseID)
+      console.log("[Controls] Get Bracket Details from Start.GG")
+
       ipcRenderer.send("update-bracketsData", {
-        topLeftText: this.$data.topLeftText,
-        r1p1name: this.$data.QualiSel,
-        r1p1score: this.$data.r1p1score,
-        r1p2name: this.$data.r1p2name,
-        r1p2score: this.$data.r1p2score,
-
-        r2p1name: this.$data.r2p1name,
-        r2p1score: this.$data.r2p1score,
-        r2p2name: this.$data.r2p2name,
-        r2p2score: this.$data.r2p2score,
-
-        winr2p1name: this.$data.winr2p1name,
-        winr2p1score: this.$data.winr2p1score,
-        winr2p2name: this.$data.winr2p2name,
-        winr2p2score: this.$data.winr2p2score,
-
-        loser1p1name: this.$data.loser1p1name,
-        loser1p1score: this.$data.loser1p1score,
-        loser1p2name: this.$data.loser1p2name,
-        loser1p2score: this.$data.loser1p2score,
-
-        loser2p1name: this.$data.loser2p1name,
-        loser2p1score: this.$data.loser2p1score,
-        loser2p2name: this.$data.loser2p2name,
-        loser2p2score: this.$data.loser2p2score
+        toornamentEventId: this.$data.toornamentEventId,
+        toornamentStageId: this.$data.toornamentStageId,
       });
     },
   },
